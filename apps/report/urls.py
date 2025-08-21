cc86ff47b03399abdd89672b0cf972c7eae03894
SanyoEntertain <sanyoentertain@gmail.com>
2025-08-21 21:30:27 +0900

    except report

--- apps/report/urls.py
+++ apps/report/urls.py
@@ -1,8 +1,9 @@
-from .views import StartReportView, SavePriceView
+from .views import StartReportView, SavePriceView, MakeReportView
 from django.urls import path
 
 urlpatterns = [
     path('startReport/', StartReportView.as_view(), name='start_report'),
     path('savePrice/', SavePriceView.as_view(), name='save_price'),
+    path('makeReport/', MakeReportView.as_view(), name='make_report'),
 ]
 
